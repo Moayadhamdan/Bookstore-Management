fdd60d7ea016d426ed2714e60e036b06ef66a804
Moayadhamdan <hamadanjo@gmail.com>
2024-09-30 00:57:47 +0300

    Bulid Bookstore Management System

---
+++ Bookstore-Management/Data/BookstoreDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Bookstore_Management.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bookstore_Management.Data
+{
+    public class BookstoreDbContext : DbContext
+    {
+        public BookstoreDbContext(DbContextOptions<BookstoreDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
